a29fd4077f2f62ff18cf5550f65865af7c3c9858
yug08 <andhariayug@gmail.com>
2024-11-05 19:47:58 +0530

    first commit

---
+++ packages/js/ala.js
@@ -0,0 +1,23 @@
+// var fruits = {"apple":1, "banana":2, "orange":3};
+// console.log(fruits);
+document.getElementById("btn").onclick = function() { alert("Button clicked by Andhariya Parshav!"); };
+var regex = /hello world/;
+console.log(regex);
+let day= "Monday"
+switch (day) {
+case "Monday":
+    alert("Today is Monday");
+    break
+case "Tuesday":
+    alert("Today is Tuesday");
+    break
+}
+
+try {
+    var y = null;
+      var x = y / 0;
+    }
+    catch(e){
+    console.log(e);
+    }
+    
